df8f2f28084a4d4656f64f96b95a40f8f0582ab2
Huỳnh Văn Bình <huynhvanbinh1606@gmail.com>
2022-06-03 22:42:37 +0700

    function create with nodejs

--- CRUDwithNodejs/routes/index.js
+++ CRUDwithNodejs/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var dbConnect = require("../database/connect");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  dbConnect.query("SELECT * FROM student", function(err, data){
+    if(err) throw err;
+    res.render("index", {data: data});
+  })
+});
+
+//them hoc sinh moi
+router.get("/add", function(req, res){
+  res.render("add");
+});
+router.post("/add", function(req, res){
+  dbConnect.query(`INSERT INTO student (name, email, class) VALUES('${req.body.name}','${req.body.email}','${req.body.class}')`,function(err){
+    if(err) throw err;
+    res.redirect("/")
+  })
 });
 
 module.exports = router;
